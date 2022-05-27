568969f28f8fb65e2f90e618825841f1d227ec29
Moibe <moi_estrello@hotmail.com>
2022-05-27 04:06:47 -0500

    Countdown

--- kyc.js
+++ kyc.js
@@ -1,6 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const direccion = urlParams.get('dir');
-const IBAN = urlParams.get('IBAN');
+const nombre = urlParams.get('nombre');
 CUAL = " Verification pending, wating for documents.";
 if (direccion == "0x521c72007557063b2a61998f7f578cb752c8dcac" && IBAN == "772123456789012345") {
 CUAL = "Documents received, verifying."; 
