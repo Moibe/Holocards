62a93418d1701711faa0ce13b6d554c6ce119c17
Moibe <moi_estrello@hotmail.com>
2022-06-23 19:54:13 -0500

    Currency and locale ready OK!

--- herramientas.js
+++ herramientas.js
@@ -2,10 +2,57 @@ function obtenNombre(nombre){
 
 const [first, ...rest] = nombre.split(" "); 
 
-console.log(first); // 👉️ try
-console.log(rest); // 👉️ ['again', 'later']
+//(first) 👉️ try
+//(rest) 👉️ ['again', 'later']
 
 return first; 
+
+}
+
+function obtenFecha(locale){
+
+// program to display the date
+// get local machine date time
+const date = new Date();
+
+// get the date as a string
+const n = date.toDateString();
+console.log("Date as a string:");
+console.log(n);
+
+// get the time as a string
+const time = date.toLocaleTimeString();
+console.log("Time as a string:");
+console.log(time);
+
+// display date
+console.log('Date: ' + n);
+
+// display time
+console.log('Time: ' + time);
+
+
+// get the day from the date
+let day = date.getDate();
+console.log("Éste es el número de día:");
+console.log(day);
+
+// get the month from the date
+// + 1 because month starts from 0
+let month = date.getMonth() + 1;
+console.log("Ésto es el número de mes:");
+console.log(month);
+
+console.log("Ésta es la fecha formateada...");
+console.log(day + "," + month);
+
+mes_escrito = date.toLocaleString(locale, { month: 'long' })
+
+console.log("Éste es el resultado final que queremos!:");
+console.log(mes_escrito + " " + day);
+fecha_requerida = mes_escrito + " " + day;
+
+return fecha_requerida; 
 
 }
 
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //IDIOMA
 let jsonTraducciones;
 let idioma = 'english';
+let pais = 'Mexico';
 let nombre_completo; 
+let locale; 
+let currency = 'USD'; 
 
 //Declaramos el json que contendrá toda la información.
 let jsonTransactions;
@@ -18,10 +21,30 @@ jsonTransactions = JSON.parse(transacciones);
 //Traducciones
 jsonTraducciones = JSON.parse(traducciones);
 
-escribeNombre(txid, jsonTransactions);
+//Countries
+jsonCountries = JSON.parse(paises);
+
+pais = jsonTransactions[txid].pais;
+console.log("ESTO ES EL PAIS DEL USUARIO.");
+console.log(pais);
+
+locale = jsonCountries[pais].locale;
+console.log("Esto es el locale:");
+console.log(locale);
+
+escribeTitulo();
+escribeNombre(txid);
+escribePrecio(); 
+escribeFooter();
 }
 
-function escribeNombre(txid, jsonTransactions){
+function escribeTitulo(){
+    texto_top_title = jsonTraducciones[idioma].top_title;
+    top_title = document.getElementById('top_title');
+    top_title.innerHTML = texto_top_title;
+}
+
+function escribeNombre(txid){
 
     //Textos Fijos
     welcome1 = jsonTraducciones[idioma].welcome1;
@@ -30,11 +53,45 @@ function escribeNombre(txid, jsonTransactions){
     //Variable: Nombre.
     nombre_completo = jsonTransactions[txid].nombre;
     nombre_propio = obtenNombre(nombre_completo)
-    console.log("Nombre propio:" + nombre_propio);
-
+   
     texto_completo = welcome1 + nombre_propio + welcome2; 
     
     welcome_message = document.getElementById('welcome_message');
     welcome_message.innerHTML = texto_completo;
     
-    }
+}
+
+function escribePrecio(){
+
+    precio = 25;
+
+    //Textos Fijos
+    texto_price1 = jsonTraducciones[idioma].texto_price1;
+        
+    texto_completo = texto_price1 + precio + " " + currency;
+
+    for (let i = 1; i < 5; i++) {
+        price = document.getElementById(i);
+        price.innerHTML = texto_completo;
+      }
+    
+    
+
+}
+
+function escribeFooter(){
+    
+    //Textos Fijos
+    footer1 = jsonTraducciones[idioma].footer1;
+    footer2 = jsonTraducciones[idioma].footer2;
+
+    
+
+    fecha_limite = obtenFecha(locale);
+    texto_completo = footer1 + fecha_limite + footer2; 
+
+    
+    
+    footer = document.getElementById('footer');
+    footer.innerHTML = texto_completo;
+}
